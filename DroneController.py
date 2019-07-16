98bdc5fedde4235ccae4bf0e78efd3627760d37d
gatordevin <gatordevin@gmail.com>
2019-07-16 16:40:09 -0400

    Update DroneController.py

--- DroneController.py
+++ DroneController.py
@@ -10,22 +10,51 @@ class droneCont:
         self.armed = False
         self.data = [1500] * 16
         self.throttleDead = 1100
+        self.alignMod = 0
     def updateDead(self, change):
         self.throttleDead += change
     def move(self, y, r ,p, t):
         self.data[self.yaw] = 1500 + (y*500)
-        self.data[self.roll] = 1500 + (r*500)
+        self.data[self.roll] = 1500 + (r*500) + self.alignMod
         self.data[self.pitch] = 1500 + (p*500)
-        if(self.throttleDead + (t*500) > 10):
+
+        # self.data[self.throttle] = int(t * 500 + 1500)
+        # print(self.data[self.throttle])
+
+        if(self.throttleDead + (t*500) > 900):
             self.data[self.throttle] = self.throttleDead + (t * 500)
         else:
-            self.data[self.throttle] = 1000
-        print(self.data[self.throttle])
+            self.data[self.throttle] = 900
+
+
         if(self.armed == True):
             self.data[self.arm] = 2000
         else:
             self.data[self.arm] = 1200
         self.sbus.sendUDP(self.data)
+    def moveRaw(self, y, r ,p, t):
+        self.data[self.yaw] = y
+        self.data[self.roll] = r
+        self.data[self.pitch] = p
+        self.data[self.throttle] = t
+        if(self.armed == True):
+            self.data[self.arm] = 2000
+        else:
+            self.data[self.arm] = 1200
+
+    def enableAlignment(self):
+        data = bytearray(4)
+        data[0] = ord('F')
+        data[1] = ord('A')
+        data[2] = ord('C')
+        data[3] = ord('E')
+        try:
+            self.sbus.client_socket.sendto(data, (self.sbus.ip, 6666))
+            self.sbus.client_socket.settimeout(0.01)
+            data = self.sbus.client_socket.recvfrom(256)
+        except:
+            None
+
     def send(self,channel):
         data = channel
         self.sbus.sendUDP(data)
@@ -35,11 +64,8 @@ class droneCont:
     def arming(self):
         data = [1500] * 16
         data[self.arm] = 2000
-        data[self.throttle] = 800.0
         self.armed = True
-        self.sbus.sendUDP(data)
     def disarm(self):
         data = [1500] * 16
         data[self.arm] = 1200
         self.armed = False
-        self.sbus.sendUDP(data)
